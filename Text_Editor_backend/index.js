6bb37ce2c7408c934ca4e58bd8633d5e4b9894f2
Shubham <itsmeshubham2799@gmail.com>
2024-05-02 23:38:35 +0530

    added some changes

--- Text_Editor_backend/index.js
+++ Text_Editor_backend/index.js
@@ -1,18 +1,19 @@
 const Express = require('express')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
-const dotenv =  require('dotenv')
+const dotenv = require('dotenv')
 const cors = require('cors');
 
 dotenv.config()
 
-const db = require('./config/db')
+const db = require('./config/db');
+db();
 
 const dataRoutes = require('./Routes/routes')
 
-const app =  Express();
+const app = Express();
 
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT || 3001
 
 
 
@@ -23,31 +24,27 @@ app.use(cors());
 
 
 //testing ----------------->>>>>>>>>>>>>>>>>>
-app.get('/test',(req,res)=>
-{
+app.get('/test', (req, res) => {
     res.send("hello world")
 })
 
 
 //error handling--------------->>>>>>>>>>>>>>>>
 
-app.use((err,req,res,next)=>
-{
+app.use((err, req, res, next) => {
     res.status(404).send("can't find that!");
 })
 
-app.use((err,req,res,next)=>
-{
+app.use((err, req, res, next) => {
     res.status(500).send("Sorry! something breaks")
 })
 
 //routes are here ------------------>>>>>>>>>>>>>>>>>>>>>>
-app.use('/v1',dataRoutes);
+app.use('/v1', dataRoutes);
 
 
 
 // listening ------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-app.listen(PORT,()=>
-{
+app.listen(PORT, () => {
     console.log(`the server is listening on ${PORT}`)
 })
